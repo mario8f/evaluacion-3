1b8c880b4a5f6d3dbdb5f46ff65e5e18e6d21f22
mario8f <55576317+mario8f@users.noreply.github.com>
2020-02-27 09:25:35 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cont;
+		int numero=5;
+		System.out.println("Hola mundo");
+		for(cont=0;cont<=10;cont++){
+			System.out.print(cont);
+			System.out.print(" x ");
+			System.out.print(numero);
+			System.out.print(" = ");
+			System.out.println(cont*numero);
+			
+		}
+        
+	}
+
+}
